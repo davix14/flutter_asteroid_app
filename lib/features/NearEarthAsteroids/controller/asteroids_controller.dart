888e5cebc3e8cfe1ab3e8b46066ae86af959aa50
divrex <jdavidjuarez@gmail.com>
2023-07-17 00:03:43 -0600

    - changed date input to only allow date range of last 7 days to be chosen

--- lib/features/NearEarthAsteroids/controller/asteroids_controller.dart
+++ lib/features/NearEarthAsteroids/controller/asteroids_controller.dart
@@ -1,8 +1,9 @@
 import 'package:asteroid_test_app/features/NearEarthAsteroids/models/nea_model.dart';
 import 'package:asteroid_test_app/features/NearEarthAsteroids/service/nea_service.dart';
-import 'package:asteroid_test_app/util/helpers.dart';
 import 'package:asteroid_test_app/util/local_storage_repository.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:jiffy/jiffy.dart';
 
 final asteroidsControllerProvider = StateNotifierProvider.autoDispose<
     AsteroidsController,
@@ -17,20 +18,38 @@ class AsteroidsController extends StateNotifier<AsyncValue<NeaModel>> {
   NeaModel? latestAsteroids;
 
   NeaService get _neaService => _ref.read(neaServiceProvider);
-  String lastDate = getFormattedDate(DateTime.now());
+  late DateTimeRange lastDate;
 
-  void getAsteroids({DateTime? dateIn}) async {
+  void getAsteroids({DateTimeRange? dateRangeIn}) async {
     state = const AsyncLoading();
-    lastDate = dateIn != null
-        ? lastDate = getFormattedDate(dateIn)
-        : _ref.read(localStorageProvider).lastDateUsed;
-    if (lastDate == '') lastDate = getFormattedDate(DateTime.now());
     try {
+      if (_ref.read(localStorageProvider).lastStartDateUsed == '') {
+        lastDate = DateTimeRange(
+            start: Jiffy.now().subtract(days: 7).dateTime, end: DateTime.now());
+      } else {
+        lastDate = dateRangeIn != null
+            ? lastDate = dateRangeIn
+            : DateTimeRange(
+                start: DateTime.parse(
+                    _ref.read(localStorageProvider).lastStartDateUsed),
+                end: DateTime.parse(
+                    _ref.read(localStorageProvider).lastEndDateUsed));
+      }
       latestAsteroids = await _neaService.getNea(lastDate);
       state = AsyncValue.data(latestAsteroids!);
     } catch (e, stacktrace) {
       print('error getting asteroids: $stacktrace');
       state = AsyncValue.error(e, stacktrace);
     }
+  }
+
+  List<SingleAsteroid>? getAllAsteroids(NeaModel neaM) {
+    final List<SingleAsteroid>fullList = [];
+    for (final day in neaM.asteroidList.values) {
+      for (final single in day) {
+        fullList.add(single);
+      }
+    }
+    return fullList;
   }
 }
